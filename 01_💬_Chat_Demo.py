248ac10dd76999f9a989313cad5997c0d3d4391b
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2024-03-29 16:48:32 -0400

    Add emojis for chat

--- 01_💬_Chat_Demo.py
+++ 01_💬_Chat_Demo.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 from openai import OpenAI
 
-st.set_page_config(page_title="Ollama x Streamlit UI",
-                   page_icon="🎈",
-                   layout="wide",
-                   initial_sidebar_state="expanded"
-                   )
+st.set_page_config(
+    page_title="Ollama x Streamlit UI",
+    page_icon="🎈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 
 def icon(emoji: str):
@@ -32,11 +33,15 @@ def extract_model_names(models_info: list) -> tuple:
 
     :param models_info: A dictionary containing the models' information.
 
-    Return: 
+    Return:
         A tuple containing the model names.
     """
 
-    return tuple(model['name'] for model in models_info['models'] if model['name'].lower() != 'llava:latest')
+    return tuple(
+        model["name"]
+        for model in models_info["models"]
+        if model["name"].lower() != "llava:latest"
+    )
 
 
 def main():
@@ -45,22 +50,27 @@ def main():
     """
 
     icon("🚀")
-    st.subheader("Ollama Playground", divider="gray", anchor=False)
+    st.subheader("Ollama Playground", divider="red", anchor=False)
 
     client = OpenAI(
-        base_url='http://localhost:11434/v1',
-        api_key='ollama',  # required, but unused
+        base_url="http://localhost:11434/v1",
+        api_key="ollama",  # required, but unused
     )
 
     models_info = ollama.list()
     available_models = extract_model_names(models_info)
 
     if available_models:
-        selected_model = st.selectbox('Pick a model available locally on your system ↓',
-                                      available_models)
+        selected_model = st.selectbox(
+            "Pick a model available locally on your system ↓", available_models
+        )
 
     else:
         st.warning("You have not pulled any model from Ollama yet!", icon="⚠️")
+        # if st.toggle("Toggle to download model"):
+        #     with st.spinner("Pulling model"):
+        #         ollama.pull(model)
+
         st.page_link("https://ollama.com/library",
                      label="Find models here", icon="🤖")
         st.stop()
@@ -71,7 +81,8 @@ def main():
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with message_container.chat_message(message["role"]):
+        avatar = '🤖' if message["role"] == "assistant" else '😎'
+        with message_container.chat_message(message["role"], avatar=avatar):
             st.markdown(message["content"])
 
     if prompt := st.chat_input("Enter a prompt here..."):
@@ -79,9 +90,9 @@ def main():
             st.session_state.messages.append(
                 {"role": "user", "content": prompt})
 
-            message_container.chat_message("user").markdown(prompt)
+            message_container.chat_message("user", avatar='😎').markdown(prompt)
 
-            with message_container.chat_message("assistant"):
+            with message_container.chat_message("assistant", avatar='🤖'):
                 with st.spinner("wait for it..."):
                     stream = client.chat.completions.create(
                         model=selected_model,
--- pages/02_🌋_LLaVA_Demo.py
+++ pages/02_🌋_LLaVA_Demo.py
@@ -1,20 +1,34 @@
 import streamlit as st
 import requests
 import base64
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 from io import BytesIO
 import json
 
-st.set_page_config(page_title="LLaVA Playground", page_icon="🌋",
-                   layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    page_title="LLaVA Playground",
+    page_icon="🌋",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 
 def icon(emoji: str):
     st.write(
-        f'<span style="font-size: 78px; line-height: 1">{emoji}</span>', unsafe_allow_html=True)
+        f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
+        unsafe_allow_html=True,
+    )
 
 
 def img_to_base64(image):
+    """
+    Convert an image to base64 format.
+
+    Args:
+        image: PIL.Image - The image to be converted.
+    Returns:
+        str: The base64 encoded image.
+    """
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue()).decode()
@@ -32,16 +46,17 @@ def generate_llava_responses(response_text):
 
 def main():
     icon("🌋")
-    st.subheader("LLaVA 1.6 Playground", divider="grey", anchor=False)
+    st.subheader("LLaVA 1.6 Playground", divider="red", anchor=False)
 
-    if 'chats' not in st.session_state:
+    if "chats" not in st.session_state:
         st.session_state.chats = []
 
-    if 'uploaded_file_state' not in st.session_state:
+    if "uploaded_file_state" not in st.session_state:
         st.session_state.uploaded_file_state = None
 
-    uploaded_file = st.file_uploader("Upload an image for analysis",
-                                     type=["png", "jpg", "jpeg"])
+    uploaded_file = st.file_uploader(
+        "Upload an image for analysis", type=["png", "jpg", "jpeg"]
+    )
 
     col1, col2 = st.columns(2)
 
@@ -59,23 +74,31 @@ def main():
 
         if uploaded_file is not None:
             for message in st.session_state.chats:
-                with container2.chat_message(message["role"]):
+                avatar = '🌋' if message["role"] == "assistant" else '🫠'
+                with container2.chat_message(message["role"], avatar=avatar):
                     st.markdown(message["content"])
 
             if user_input := st.chat_input(
-                    "Question about the image...", key="chat_input"):
+                "Question about the image...", key="chat_input"
+            ):
                 st.session_state.chats.append(
-                    {'role': 'user', 'content': user_input})
-                container2.chat_message("user").markdown(user_input)
+                    {"role": "user", "content": user_input})
+                container2.chat_message(
+                    "user", avatar='🫠').markdown(user_input)
 
                 image_base64 = img_to_base64(image)
                 API_URL = "http://localhost:11434/api/generate"
-                headers = {"Content-Type": "application/json",
-                           "Accept": "application/json"}
-                data = {"model": "llava", "prompt": user_input,
-                        "images": [image_base64]}
+                headers = {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                }
+                data = {
+                    "model": "llava",
+                    "prompt": user_input,
+                    "images": [image_base64],
+                }
 
-                with container2.chat_message("assistant"):
+                with container2.chat_message("assistant", avatar='🌋'):
                     with st.spinner("wait for it..."):
                         response = requests.post(
                             API_URL, json=data, headers=headers)
@@ -83,14 +106,15 @@ def main():
                         response_generator = generate_llava_responses(
                             response.text)
                         llava_responses = " ".join(
-                            response for response in response_generator)
+                            response for response in response_generator
+                        )
                         st.markdown(llava_responses)
                     else:
                         st.error("Failed to get a response from LLaVA.")
 
                 st.session_state.chats.append(
-                    {'role': 'assistant',
-                     'content': llava_responses})
+                    {"role": "assistant", "content": llava_responses}
+                )
 
 
 if __name__ == "__main__":
